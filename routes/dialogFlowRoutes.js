9baf13dc48ceaefc07546be538b9ecfb5a4b64dd
Pranjal Dahal <dahalpranjal18@gmail.com>
2020-10-10 20:45:51 +0530

    Error

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,4 +1,4 @@
-`use strict`
+`use-strict`;
 
 const chatbot = require('../chatbot/chatbot');
 
@@ -9,29 +9,16 @@ module.exports = app => {
     });
     
     app.post('/api/df_text_query', async (req, res) => {
-
-        try{
-            let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
-        res.send(responses[0].queryResult);
         
-        }
-        catch (error) {
-            console.log(error)
-        }
+        let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+        res.send(responses[0].queryResult);
 
     });
 
     
     app.post('/api/df_event_query', async (req, res) => {
-
-
-        try {
-            let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+        let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
         res.send(responses[0].queryResult);
-        }
-        catch (error) {
-            console.log(error)
-        }
     });    
     
 }
