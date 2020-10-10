563cafbaf943afef525b0a3b9ac9404dd68d3692
Pranjal Dahal <dahalpranjal18@gmail.com>
2020-10-10 18:35:36 +0530

    correcting error

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -8,12 +8,14 @@ module.exports = app => {
         res.send({'Hello': 'johny'})
     });
     
-    app.post('/api/df_text_query', async(req,res) => {
+    app.post('/api/df_text_query', async (req, res) => {
         let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
         res.send(responses[0].queryResult);
+
     });
+
     
-    app.post('/api/df_event_query', async (req,res) => {
+    app.post('/api/df_event_query', async (req, res) => {
         let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
         res.send(responses[0].queryResult);
     });    
